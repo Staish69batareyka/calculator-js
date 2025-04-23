5eb39e6e680a9d11232058358e9f6bea7b200049
Anastaish (Batareyka) <151395197+Staish69batareyka@users.noreply.github.com>
2025-04-23 15:08:08 +0300

    adding render in list-app

--- 02-array-object-start.html/app.js
+++ 02-array-object-start.html/app.js
@@ -2,23 +2,44 @@ const title = document.getElementById('title')
 const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
 
-createBtn.onclick = () =>{
-    
-        listElement.insertAdjacentHTML(
-            'beforeend', 
-            `
-        <li
-          class="list-group-item d-flex justify-content-between align-items-center"
-        >
-          <span>${title.value}</span>
-          <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
-          </span>
-        </li>
-        
-        `
 
-        )
+const notes = ['xmmm', 'dfdfd']
+
+
+function list(element){
+    return `
+    <li
+      class="list-group-item d-flex justify-content-between align-items-center"
+    >
+      <span>${element}</span>
+      <span>
+        <span class="btn btn-small btn-success">&check;</span>
+        <span class="btn btn-small btn-danger">&times;</span>
+      </span>
+    </li>
+    
+    `
+    
+}
+
+function render(){
+    // for(let i = 0; i < notes.length; i++){
+    //     listElement.insertAdjacentHTML('beforeend', list(notes[i]))
+    // }
+
+    for (let note of notes){
+        listElement.insertAdjacentHTML('beforeend', list(note))
+    }
+        
+}
+render()
+
+
+createBtn.onclick = () =>{
+    if(!title.value) {
+        return
+    }
+    listElement.insertAdjacentHTML('beforeend', list(title.value))
+    title.value = ''
 
 }
