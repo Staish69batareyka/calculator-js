c9a446f26df1b04ea3c9998cd190c8987ede3974
Anastaish (Batareyka) <151395197+Staish69batareyka@users.noreply.github.com>
2025-04-23 20:25:01 +0300

    date-time

---
+++ 03-theory-num-str-date/app.js
@@ -0,0 +1,38 @@
+const output = document.getElementById('output')
+const fullBtn = document.getElementById('full')
+const dateBtn = document.getElementById('date')
+const timeBtn = document.getElementById('time')
+
+let mode = 'time'
+
+function bindMode(name) {
+    return function() {
+        mode = name
+        update()
+    }
+}
+
+fullBtn.onclick = bindMode('full')
+dateBtn.onclick = bindMode('date')
+timeBtn.onclick = bindMode('time')
+
+update()
+
+setInterval(update, 1000)
+
+function update(){
+    output.textContent = format(mode)
+}
+
+function format(mode) {
+
+    const now = new Date()
+
+    switch(mode){
+        case 'time': return now.toLocaleTimeString()
+        case 'date': return now.toLocaleDateString()
+        case 'full': return now.toLocaleDateString() + ' ' + now.toLocaleTimeString()
+        default: return now.toLocaleTimeString()
+    }
+
+}
